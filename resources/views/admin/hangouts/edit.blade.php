a9d42ebee1358e7461b2ad56f98fcc3977ce5f8b
muhbase <67451498+takanesachio@users.noreply.github.com>
2025-05-26 13:41:31 +0700

    improve data

--- resources/views/admin/hangouts/edit.blade.php
+++ resources/views/admin/hangouts/edit.blade.php
@@ -155,7 +155,7 @@
                                     <h5>Upload Gambar Tambahan</h5>
                                 </div>
                                 <div class="card-body">
-                                    <form action="{{ route('hangout.upload') }}" method="POST" class="dropzone"
+                                    <form action="{{ route('hangout.upload.image') }}" method="POST" class="dropzone"
                                         id="dropzone">
                                         @csrf
                                     </form>
